ac1ef0441d8bf6641d1efb103f58c4ab2db86200
Genius_um <101275371+Geniusum@users.noreply.github.com>
2024-04-09 17:57:35 +0200

    Add files via upload

---
+++ mazegroup/README.md
@@ -0,0 +1,5 @@
+# MazeGroup.py
+MazeGroup.py is an general prupose library for Python.
+
+[GitHub](https://github.com/Geniusum/mazegroup.py)
+[MazeGroup](https://mazegroup.org/)
---
+++ mazegroup/mazegroup/__init__.py
@@ -0,0 +1,7 @@
+"""
+# MazeGroup.py
+MazeGroup.py is an general prupose library for Python.
+"""
+
+class Greet():
+    def __init__(self) -> None: print("Hello! Welcome in MazeGroup.py")
