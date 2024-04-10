75fdcc88165a20a322c0792d9527beb14a6fad43
Genius_um <101275371+Geniusum@users.noreply.github.com>
2024-04-10 09:28:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mazegroup",
-    version="0.1.2",
+    version="0.1.2.1",
     description="MazeGroup.py is an general prupose library for Python.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
