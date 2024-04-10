6e53bc078b592af4151cdcb27212c749fa4cd2b2
Genius_um <101275371+Geniusum@users.noreply.github.com>
2024-04-10 08:47:15 +0200

    Delete mazegroup directory

--- mazegroup/README.md
+++
@@ -1,5 +0,0 @@
-# MazeGroup.py
-MazeGroup.py is an general prupose library for Python.
-
-[GitHub](https://github.com/Geniusum/mazegroup.py)
-[MazeGroup](https://mazegroup.org/)
--- mazegroup/mazegroup/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-# MazeGroup.py
-MazeGroup.py is an general prupose library for Python.
-"""
-
-class Greet():
-    def __init__(self) -> None: print("Hello! Welcome in MazeGroup.py")
