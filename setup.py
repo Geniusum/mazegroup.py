b2b6869e115f698d1473139a1a786d8bb9e07bac
Genius_um <101275371+Geniusum@users.noreply.github.com>
2024-04-10 08:47:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Genius_um & Rayanis55",
     python_requires=">=3.9",
     url="https://github.com/Geniusum/mazegroup.py",
-    packages=["mazegroup"], #find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
